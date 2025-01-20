88d072a793e155d7c8bac43397f34579c90d7e7d
신희성 <vvwv3092@gmail.com>
2025-01-20 16:48:33 +0900

    update: student query by user id service transaction

--- src/main/java/gogo/gogouser/domain/student/application/StudentServiceImpl.kt
+++ src/main/java/gogo/gogouser/domain/student/application/StudentServiceImpl.kt
@@ -2,14 +2,18 @@ package gogo.gogouser.domain.student.application
 
 import gogo.gogouser.domain.student.application.dto.StudentDto
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StudentServiceImpl(
     private val studentReader: StudentReader,
     private val studentMapper: StudentMapper
 ) : StudentService {
+
+    @Transactional(readOnly = true)
     override fun queryByUserId(userId: Long): StudentDto {
         val student = studentReader.readByUserId(userId)
         return studentMapper.map(student)
     }
+
 }
